8704f608056cc45fedf26da612d52ce80479c4f6
EldarSaidnazarov <eldarsaidnazarov@gmail.com>
2025-10-02 14:32:55 +0500

    Update Files Homework.py

--- Files Homework.py
+++ Files Homework.py
@@ -1,6 +1,6 @@
 with open('data/pi_million_digits.txt') as file:
     pi = file.read().replace('\n','').strip()
-    birthday = '12042005'
+    birthday = '09437027'
     find = pi.find(birthday)
     if find != -1:
         print(f"Дата {birthday} найдена в числе pi на позиции {find}")
@@ -9,11 +9,11 @@ with open('data/pi_million_digits.txt') as file:
 
 import pickle
 pi_float = float(pi[:100])
-with open('pi_float.pkl', 'wb') as f:
+with open('pi_float', 'wb') as f:
     pickle.dump(pi_float, f)
     print("Число pi сохранено в формате float с помощью pickle.")
 
 # Проверка: загрузи число из pickle и выведи
-with open('pi_float.pkl', 'rb') as f:
+with open('pi_float', 'rb') as f:
     loaded_pi = pickle.load(f)
     print("Загруженное число pi из файла:", loaded_pi)
